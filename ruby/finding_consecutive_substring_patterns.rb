5bd0c34873c73df02a97fb0615e4aa97080a5189
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2023-11-11 15:05:31 -0600

    Adds a simpler solution for the same problem on consecutive substrings

--- ruby/finding_consecutive_substring_patterns.rb
+++ ruby/finding_consecutive_substring_patterns.rb
@@ -3,7 +3,7 @@
 # 1 - the rest of the consonants without vowels and y
 
 # For example
-# find_pattern_matching_count('010', 'amazing')
+# find_pattern_matching_count_second('010', 'amazing')
 # Should return 2
 # because vowel constant vowel
 # amazing - ama - matches
@@ -15,7 +15,7 @@
 # g
 
 # Another example
-# find_pattern_matching_count('100', 'codesignal')
+# find_pattern_matching_count_second('100', 'codesignal')
 # Should return 0 matches
 # because constant vowel vowel
 # codesignal
@@ -29,10 +29,45 @@
 # al
 # l
 
+## First solution
+def find_pattern_matching_count_first(pattern, string_value)
+  vowels = 'aeiouy'
+
+  # convert the string into a pattern of zeros and ones
+  string_pattern = string_value.chars.map do |character|
+    vowels.include?(character) ? '0' : '1'
+  end.join
+
+  counter = 0
+
+  positions = pattern.size
+  interactions = string_with_patterns.size - positions + 1
+  interactions.times do |index|
+    counter += 1 if string_with_patterns[index, positions]
+  end
+
+  # or
+  # (0..string_pattern.size - pattern.size).each do |index|
+  #   counter += 1 if string_pattern[index, pattern.size] == pattern
+  # end
+
+  counter
+end
+
+puts '*' * 100
+p "find_pattern_matching_count_first('100', 'codesignal') #{find_pattern_matching_count_first('100', 'codesignal')}" # 2
+p "find_pattern_matching_count_first('010', 'amazing') #{find_pattern_matching_count_first('010', 'amazing')}" # 0
+p "find_pattern_matching_count_first('010', 'anotheramazing') #{find_pattern_matching_count_first('010', 'anotheramazing')}" # 4
+p "find_pattern_matching_count_first('010', 'ayasee') #{find_pattern_matching_count_first('010', 'ayasee')}" # 1(
+
+puts '*' * 100
+
+## Second solution
+
 VOWELS_AND_Y = %w[a e i o u y].freeze
 CONSONANTS = ('b'..'z').to_a - VOWELS_AND_Y
 
-def find_pattern_matching_count(pattern, string_value)
+def find_pattern_matching_count_second(pattern, string_value)
   return 0 if string_value.to_s.empty?
 
   matching_pattern = create_matching_pattern(pattern)
@@ -57,7 +92,7 @@ def create_matching_pattern(pattern)
   "^#{final_pattern.join('')}"
 end
 
-p "find_pattern_matching_count('100', 'codesignal') #{find_pattern_matching_count('100', 'codesignal')}" # 2
-p "find_pattern_matching_count('010', 'amazing') #{find_pattern_matching_count('010', 'amazing')}" # 0
-p "find_pattern_matching_count('010', 'anotheramazing') #{find_pattern_matching_count('010', 'anotheramazing')}" # 4
-p "find_pattern_matching_count('010', 'ayasee') #{find_pattern_matching_count('010', 'ayasee')}" # 1(
+p "find_pattern_matching_count_second('100', 'codesignal') #{find_pattern_matching_count_second('100', 'codesignal')}" # 2
+p "find_pattern_matching_count_second('010', 'amazing') #{find_pattern_matching_count_second('010', 'amazing')}" # 0
+p "find_pattern_matching_count_second('010', 'anotheramazing') #{find_pattern_matching_count_second('010', 'anotheramazing')}" # 4
+p "find_pattern_matching_count_second('010', 'ayasee') #{find_pattern_matching_count_second('010', 'ayasee')}" # 1(
