f56f74c4b53d0ffecbdd0b97005b4f94e223ec61
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2024-05-24 21:15:03 -0600

    Practicing with load path LOAD_PATH

---
+++ ruby/load_path_practice/load_path.rb
@@ -0,0 +1,8 @@
+puts "*" * 100
+puts "LOAD_PATH"
+puts $LOAD_PATH
+puts "*" * 100
+puts 'File.dirname(__FILE__)'
+puts File.dirname(__FILE__)
+puts "*" * 100
+puts File.expand_path('../load_path_practice', __FILE__)
